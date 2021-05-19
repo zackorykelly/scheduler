5d0eba9b5b53633151f096e4ee57dc37de7e56f8
Zackory Kelly <zackkelly@live.ca>
2021-05-19 19:04:22 -0400

    Add basic websocket framework

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -21,6 +21,14 @@ export default function useApplicationData() {
       axios.get('/api/interviewers')
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
+      const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+      webSocket.onopen = function (event) {
+        webSocket.send('ping');
+      };
+      webSocket.onmessage = function (event) {
+        const data = JSON.parse(event.data);
+        console.log(data);
+      }
     })
   }, []);
 
